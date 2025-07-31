2d93bcc15a9d6658f6c62a5d18b58a9cb7eb2f64
strelkovsky <strelkin7@gmail.com>
2025-07-31 17:11:33 +0300

    configure configs

--- Assets/BuildConfig.cs
+++ Assets/BuildConfig.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BuildConfig", menuName = "Build Config")]
 public class BuildConfig : ScriptableObject
 {
+    [SerializeField] private string _buildName;
+    
     public void Build()
     {
         Debug.Log("Starting Unity build...");
 
         string[] scenes = new[] { "Assets/Scenes/SampleScene.unity" };
-        string buildPath = "Builds/Windows/MyGame.exe";
+        string buildPath = $"Builds/Windows/{_buildName}.exe";
 
         BuildPipeline.BuildPlayer(scenes, buildPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
 
